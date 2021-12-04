414a714b786a6801b42cc5c8ff9853452a530a2a
Vishwas <vishwas.g5@gmail.com>
2021-12-04 13:33:08 +0530

    Active Links

--- react-router-demo/src/components/Navbar.js
+++ react-router-demo/src/components/Navbar.js
@@ -1,10 +1,21 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? 'bold' : 'normal',
+      textDecoration: isActive ? 'none' : 'underline'
+    }
+  }
+
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
+      <NavLink to='/' style={navLinkStyles}>
+        Home
+      </NavLink>
+      <NavLink to='/about' style={navLinkStyles}>
+        About
+      </NavLink>
     </nav>
   )
 }
