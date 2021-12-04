8511c2629bf79315fae0592b61eb8ad2f72f6580
Vishwas <vishwas.g5@gmail.com>
2021-12-04 11:21:54 +0530

    Configuring routes

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './components/Home'
+import { About } from './components/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='about' element={<About />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
