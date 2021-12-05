30223bdeb441d774372cbdcdfe24a0a9adf926c7
Vishwas <vishwas.g5@gmail.com>
2021-12-05 17:15:04 +0530

    Index route

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Route path='about' element={<About />} />
         <Route path='order-summary' element={<OrderSummary />} />
         <Route path='products' element={<Products />}>
+          <Route index element={<FeaturedProducts />} />
           <Route path='featured' element={<FeaturedProducts />} />
           <Route path='new' element={<NewProducts />} />
         </Route>
