6478b56b8164ca6779a0e0f6e6ce97f6c7c52958
Vishwas <vishwas.g5@gmail.com>
2021-12-06 14:41:37 +0530

    URL Params

--- react-router-demo/src/components/UserDetails.js
+++ react-router-demo/src/components/UserDetails.js
@@ -1,3 +1,6 @@
+import { useParams } from 'react-router-dom'
+
 export const UserDetails = () => {
-  return <div>Details about user</div>
+  const { userId } = useParams()
+  return <div>Details about user {userId}</div>
 }
--- react-router-demo/src/components/Users.js
+++ react-router-demo/src/components/Users.js
@@ -1,11 +1,25 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useSearchParams } from 'react-router-dom'
 
 export const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const status = searchParams.get('status')
   return (
     <>
-      <h2>User 1</h2>
-      <h2>User 2</h2>
-      <h2>User 3</h2>
+      <div>
+        <span>Filters: </span>
+        <button onClick={() => setSearchParams({})}>All Users</button>
+        <button onClick={() => setSearchParams({ status: 'active' })}>
+          Active Users
+        </button>
+      </div>
+      {status === 'active' ? (
+        <>
+          <h2>User 1 - active</h2>
+          <h2>User 2 - active</h2>
+        </>
+      ) : (
+        <h2>User 3 - inactive</h2>
+      )}
       <Outlet />
     </>
   )
